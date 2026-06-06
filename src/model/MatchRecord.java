e2b37654178916ea972b490d2df03d22176a99de
Nicole1230-sky <2896162520@qq.com>
2026-06-06 15:31:28 +0800

    [AI-Implementation] implement MatchRecord.java

--- src/model/MatchRecord.java
+++ src/model/MatchRecord.java
@@ -1,10 +1,25 @@
 package model;
 
 public class MatchRecord {
+    private String teamA;
+    private String teamB;
+    private String winningTeam;
+    private String mvpHero;
+    private int durationMinutes;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public MatchRecord(String teamA, String teamB, String winningTeam, String mvpHero, int durationMinutes) {
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.winningTeam = winningTeam;
+        this.mvpHero = mvpHero;
+        this.durationMinutes = durationMinutes;
+    }
 
-	}
+    public String getWinningTeam() {
+        return winningTeam;
+    }
 
+    public String getMvpHero() {
+        return mvpHero;
+    }
 }
